cd1f4dd4bd490eb0c5f630deea6bdfd643c465d1
pradipkumarmadheshiya <prdpmadheshiya@gmail.com>
2024-01-21 23:35:34 +0530

    Accuracy and speed corrections

--- terminal_based_typing_master.py
+++ terminal_based_typing_master.py
@@ -13,7 +13,12 @@ while True:
 
     print("Choose time among 1, 2, 3, 4, 5 (minutes)")
     print("Your time choice: ",end="")
-    userChoice=int(input())
+    try:
+        userChoice=int(input())
+    except:
+        print("Please type the key")
+        userChoice=int(input())
+        print()
     print()
 
     paragraph=random.choice(test)
@@ -34,18 +39,43 @@ while True:
 
     error=0
     cnt=1
+    curWords=0
     for i in range(len(type)):
         if cnt>len(paragraph):
             break
         if paragraph[i]!=type[i]:
             error+=1
+        else:
+            curWords+=1
         cnt+=1
         if userChoice*60==timeT:
             break
+    
+    accur=len(type)-error
+    try:
+        accuracy=round((curWords/len(paragraph))*100)
+    except:
+        accuracy=0
 
     print()
-    print("Error: ",error)
-    print("taken time",timeT)
-    temp=len(type)/timeT
-    wpm=round(temp*60)
+    print("Taken time: ",timeT,"seconds")
+    print("Errors:",error,"words")
+    print("accuracy:",str(accuracy)+"%")
+    mint=timeT/60
+    wpm=round(len(type)/mint)
     print("Typing Speed:",wpm,"wpm")
+
+    print()
+    print("If you want to continue the test, press c else press q.")
+    inp=input()
+    if inp=="q" or inp=="Q":
+        print()
+        print("Thank You!")
+        print()
+        break
+    elif inp=="c" or inp=="C":
+        pass
+    else:
+        print("You have pressed the wrong key.")
+        print()
+        break
